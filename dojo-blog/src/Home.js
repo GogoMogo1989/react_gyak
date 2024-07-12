449c5e0f4d72bf612a7a5226a2d2bb39c6880d6c
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2024-07-12 18:51:44 +0200

    20240712

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,17 +1,21 @@
+import {useState} from 'react'
+import BlogList from './blogList';
+
 const Home = () => {
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ])
 
-    const handleClick = () => {
-        console.log('hello')
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id)
+        setBlogs(newBlogs)
     }
 
-    const handleClickAgain = (name) => {
-        console.log('hello'+ name)
-    }
     return ( 
         <div className='home'>
-            <h1>Home</h1>
-            <button onClick={handleClick}>Click meg</button>
-            <button onClick={()=>handleClickAgain('mario')}>Click me again</button>
+            <BlogList blogs={blogs} handleDelete={handleDelete} />
         </div>
      );
 }
