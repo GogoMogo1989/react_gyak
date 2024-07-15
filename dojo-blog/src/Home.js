fa364208ed491ec29559d03e40e89ee2604241ce
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2024-07-15 15:32:48 +0200

    20#

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,33 +1,8 @@
-import {useState, useEffect} from 'react'
 import BlogList from './blogList';
+import useFetch from './Usefetch';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null)
-    const [isPending, setIsPending] = useState(true)
-    const [error, setError] = useState(null)
-
-    useEffect(() => {
-       setTimeout(() => {
-            fetch('http://localhost:8000/blogs')
-            .then(res => {
-                if(!res.ok){
-                    throw Error('valami elromlott');
-                }
-                return res.json()
-            })
-            .then((data) => {
-                setBlogs(data);
-                setIsPending(false)
-                setError(null)
-            })
-            .catch((err) => {
-                setIsPending(false)
-                setError(err.message)
-                
-            })
-       }, 1000)
-    }, []) 
- 
+    const{ data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs')
     return ( 
         <div className='home'>
             {error && <div>{error}</div>}
