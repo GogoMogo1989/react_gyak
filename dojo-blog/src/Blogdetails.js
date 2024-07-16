bc4e19b7cbde28f0a5c1983295e533b0462ba896
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2024-07-16 17:35:50 +0200

    #31

--- dojo-blog/src/Blogdetails.js
+++ dojo-blog/src/Blogdetails.js
@@ -6,6 +6,14 @@ const BlogDetails = () => {
     const { id } = useParams()
     const{ data: blog, isPending, error} = useFetch('http://localhost:8000/blogs/' + id)
 
+    const handleClick = () => {
+        fetch('http://localhost:8000/blogs/' + blog.id, {
+            method: 'DELETE',
+        }).then(()=>{
+            console.log('blog deleted')
+        })
+    }
+
     return ( 
         <div className="blog-details">
             {error && <div>{error}</div>}
@@ -15,6 +23,7 @@ const BlogDetails = () => {
                     <h2>{ blog.title}</h2>
                     <p>Written by: { blog.author}</p>
                     <div>{ blog.body }</div>
+                    <button onClick={handleClick}>Delete</button>
                 </article>
             )}
         </div>
